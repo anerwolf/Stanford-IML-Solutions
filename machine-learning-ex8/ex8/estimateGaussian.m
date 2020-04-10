75ab470e5f2bf0184a969f6012528f0f887bfc9a
Aner Wolfenfeld <anerwolf@gmail.com>
2020-04-10 14:22:08 +0300

    test

--- machine-learning-ex8/ex8/estimateGaussian.m
+++ machine-learning-ex8/ex8/estimateGaussian.m
@@ -18,7 +18,7 @@ sigma2 = zeros(n, 1);
 % Instructions: Compute the mean of the data and the variances
 %               In particular, mu(i) should contain the mean of
 %               the data for the i-th feature and sigma2(i)
-%               should contain variance of the i-th feature.
+%               should contain variance of the i-th feature
 %
 
 
